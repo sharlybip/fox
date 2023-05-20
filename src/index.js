d0502e4a70cbbcecc91318b044164d0a71130eb5
Carlos Dominguez <sahrlybip/@gmail.com>
2023-05-19 19:46:39 -0600

    preparando gh

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage } from "./lazy";
 
-console.log('Happy hacking :)')
+const appNode = document.getElementById('images');
+const addbutton = document.querySelector('button');
+const deletebutton = document.querySelector('.delete');
+
+
+const nodeImage = () => {
+    const container = document.createElement('div');
+    
+    container.className = 'p-4 bg-black w-96 mx-auto ';
+    container.height = '450';
+    container.width = '342';
+    const img = document.createElement('img');
+    img.className = 'mx-auto bg-slate-300 rounded-md';
+    let url;
+    window.fetch('https://randomfox.ca/floof')
+    .then(rta => rta.json())
+    .then(rta => {
+        img.dataset.src = `${rta.image}`
+    });
+    img.width = '320';
+    img.style.objectFit = 'cover'
+    container.append(img);
+    return container;
+}
+
+let imagenes = 0;
+const loadImage = () => {
+    imagenes +=1;
+    console.log('Imagenes',imagenes)
+    const newImage = nodeImage();
+    appNode.append(newImage);
+    registerImage(newImage);
+    
+    
+};
+addbutton.addEventListener('click', loadImage)
+deletebutton.addEventListener('click', () => {
+    appNode.innerText = '';
+    imagenes = 0;
+})
